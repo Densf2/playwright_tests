608fdeace6daf47c573587341913b30de057d364
Denys Pavlyuk <densf2@gmail.com>
2022-04-07 19:26:46 +0300

    changed_file_with_test_due_to_issue_with_linter

--- jest.playwright.test.js
+++ jest.playwright.test.js
@@ -1,45 +1,45 @@
-const { chromium } = require('playwright');
+const {chromium} = require('playwright');
 const expect = require('expect');
-const { JobsPage } = require('./test_helpers/pages/jobs_page');
+const {JobsPage} = require('./test_helpers/pages/jobs_page');
 const Data = require('./test_helpers/test_data/text');
 
 beforeAll(async () => {
-    browser = await chromium.launch();
-})
+	browser = await chromium.launch();
+});
 afterAll(async () => {
-    await browser.close();
-})
+	await browser.close();
+});
 beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto('https://dou.ua/');
-})
+	page = await browser.newPage();
+	await page.goto('https://dou.ua/');
+});
 afterEach(async () => {
-    await page.close();
-})
+	await page.close();
+});
 
 describe('Tests', () => {
-    test('Open main page', async () => {
-        expect(await page.title()).toBe(Data.title_of_main_page);
-    });
+	test('Open main page', async () => {
+		expect(await page.title()).toBe(Data.title_of_main_page);
+	});
 
-    test('Click on salary link', async () => {
-        await page.click('text=Зарплати');
-        expect(await page.title()).toBe(Data.tile_of_salary_page);
-    });
+	test('Click on salary link', async () => {
+		await page.click('text=Зарплати');
+		expect(await page.title()).toBe(Data.tile_of_salary_page);
+	});
 
-    test('Check page Jobs', async () => {
-        const jobspage = new JobsPage(page);
-        await page.click('text=Робота');
-        expect(await page.title()).toBe(Data.title_of_vacancies_page);
-        await jobspage.searchInput.isVisible()
-    });
+	test('Check page Jobs', async () => {
+		const jobspage = new JobsPage(page);
+		await page.click('text=Робота');
+		expect(await page.title()).toBe(Data.title_of_vacancies_page);
+		await jobspage.searchInput.isVisible();
+	});
 
-    test('Check calendar page search', async () => {
-        await page.click('text=Календар');
-        expect(await page.title()).toBe(Data.title_if_calendar_page);
-        await page.locator('select[name="city"]').selectOption('https://dou.ua/calendar/city/online/');
-        expect(await page.url()).toBe('https://dou.ua/calendar/city/online/');
-        await page.locator('select[name="tag"]').selectOption('https://dou.ua/calendar/tags/QA/online/');
-        expect(await page.url()).toBe('https://dou.ua/calendar/tags/QA/online/');
-    });
-})
+	test('Check calendar page search', async () => {
+		await page.click('text=Календар');
+		expect(await page.title()).toBe(Data.title_if_calendar_page);
+		await page.locator('select[name="city"]').selectOption('https://dou.ua/calendar/city/online/');
+		expect(await page.url()).toBe('https://dou.ua/calendar/city/online/');
+		await page.locator('select[name="tag"]').selectOption('https://dou.ua/calendar/tags/QA/online/');
+		expect(await page.url()).toBe('https://dou.ua/calendar/tags/QA/online/');
+	});
+});
